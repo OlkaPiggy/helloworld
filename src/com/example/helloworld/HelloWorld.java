84e30412a9ff22742e8cb748f415cf695170623b
olha.sopotnytska <olha.sopotnytska.pz.2020@lpnu.ua>
2022-07-19 16:16:06 +0300

    registration, vers 1

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,8 +1,24 @@
 package com.example.helloworld;
+import java.util.Scanner;
 
 public class HelloWorld {
+    private static String name;
+    private static int age;
+
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("What's your name? Type here: ");
+        name = in.nextLine();
+        System.out.printf("Hello, %s \n", name);
+
+        System.out.print("How old are you? Type here: ");
+        age=in.nextInt();
+        System.out.printf("You're %d years old!\n", age);
+
+        in.close();
+
     }
 }
